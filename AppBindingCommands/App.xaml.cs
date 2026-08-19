81d7984ddcb30ba9af79e5d4e65ab0711ad850be
Angelo Abreu <angelomello538@gmail.com>
2026-08-18 22:05:10 -0300

    aula04

--- AppBindingCommands/App.xaml.cs
+++ AppBindingCommands/App.xaml.cs
@@ -6,7 +6,7 @@
         {
             InitializeComponent();
         }
-
+        //comentario
         protected override Window CreateWindow(IActivationState? activationState)
         {
             return new Window(new MainPage()) { Title = "AppBindingCommands" };
