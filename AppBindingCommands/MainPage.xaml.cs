7fe25bbe6e2e646883741c97c739c8d9f2e1157d
Angelo Abreu <angelomello538@gmail.com>
2026-08-18 21:59:56 -0300

    Add project files.

---
+++ AppBindingCommands/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AppBindingCommands
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
